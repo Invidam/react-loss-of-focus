a084b5296ee7480995806c3401687dbaf8896b52
vidam.in <vidam.in@kakaocorp.com>
2024-02-08 21:44:53 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import {Box, Button, Modal, TextField, Typography} from "@mui/material";
+import {useState} from "react";
+import BaseModal from "./BaseModal";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+
+function App() {
+
+    const [open, setOpen] = useState(false);
+    const [text, setText] = useState("");
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+
+    const Body = () => <div>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{mt: 2}}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" defaultValue={text}
+                       onChange={(e) => {
+                           setText(e.target.value)
+                       }}/></div>;
+
+    return (<div className="App">
+            <Button onClick={handleOpen}>Open modal</Button>
+            <BaseModal open={open} handleClose={handleClose} style={style} Body={Body} />
+        </div>);
+}
+
+export default App;
---
+++ src/BaseModal.js
@@ -0,0 +1,15 @@
+import {Box, Modal, TextField, Typography} from "@mui/material";
+
+export default function BaseModal({open, handleClose, style, Body}) {
+
+    return <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+    >
+        <Box sx={style}>
+            <Body/>
+        </Box>
+    </Modal>
+}
